c59ef800978d213f7a72516fd85a767997d55199
Egor-Kolomiets <95211298+Not-Igor@users.noreply.github.com>
2026-05-13 15:17:26 +0200

    Create exersise and a new dto is added

---
+++ sql-dropbox-backend/SQLDropbox/Controllers/ExerciseController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLDropbox.Data;
+using SQLDropbox.DTO;
+using SQLDropbox.Models;
+
+namespace SQLDropbox.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ExerciseController : ControllerBase
+{
+    private readonly AppDbContext _db;
+    
+    public ExerciseController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateExercise([FromBody] ExerciseDTO dto)
+    {
+        var chapter = await _db.Chapters.FirstOrDefaultAsync(c => c.ChapterId == dto.ChapterId && c.DeletedAt == null);
+        
+        if (chapter == null)
+        {
+            return BadRequest(new { message = $"Chapter with ID {dto.ChapterId} could not be found." });
+        }
+
+        var newExercise = new Exercise
+        {
+            QuestionNL = dto.QuestionNL,
+            QuestionEN = dto.QuestionEN,
+            HintNL = dto.HintNL,
+            HintEN = dto.HintEN,
+            QueryOutput = dto.QueryOutput,
+            Chapter = chapter,
+            CreatedAt = DateTime.Now,
+
+            Solutions = dto.SolutionQueries.Select(query => new Solution
+            {
+                Query = query,
+                CreatedAt = DateTime.Now
+            }).ToList()
+        };
+        await _db.Exercises.AddAsync(newExercise);
+        await _db.SaveChangesAsync();
+        
+        return CreatedAtAction(nameof(CreateExercise), new {id = newExercise.ExerciseId});
+    }
+        
+}
