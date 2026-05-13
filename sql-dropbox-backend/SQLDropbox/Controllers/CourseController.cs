5fd3a7754a2f7bc7d95181317b00543ea7781368
Lander123Lander <lander@dirix-philips.be>
2026-05-13 10:10:20 +0200

    basic endpoints for course

--- sql-dropbox-backend/SQLDropbox/Controllers/CourseController.cs
+++ sql-dropbox-backend/SQLDropbox/Controllers/CourseController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SQLDropbox.Data;
+using SQLDropbox.Models;
 
 namespace SQLDropbox.Controllers;
 
@@ -15,8 +16,70 @@ public class CourseController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult ping()
+    public ActionResult getCourses()
     {
-        return Ok("piemel");
+        var courses = _db.Courses.Select(x => new
+        {
+            x.CourseId,
+            x.CourseNameEN,
+            x.CourseNameNL,
+            x.CourseDescriptionEN,
+            x.CourseDescriptionNL,
+            x.Deadline,
+            x.IsActive
+        }).ToList();
+
+        return Ok(courses);
+    }
+
+    [HttpPost]
+    public ActionResult addCourse()
+    {
+        var course = new Course
+        {
+            CourseNameEN = "test",
+            CourseNameNL = "test",
+            CourseDescriptionEN = "test",
+            CourseDescriptionNL = "test",
+            IsActive = true,
+            CreatedAt = DateTime.Now,
+        };
+
+        _db.Courses.Add(course);
+        _db.SaveChanges();
+
+        return Ok(course);
+    }
+
+    [HttpPut("{courseID}")]
+    public ActionResult updateCourse(int courseID)
+    {
+        var course = _db.Courses.FirstOrDefault(x => x.CourseId == courseID);
+
+        if (course == null)
+        {
+            return BadRequest("Course not found");
+        }
+
+        course.UpdatedAt = DateTime.Now;
+        _db.SaveChanges();
+
+        return Ok(course);
+    }
+
+    [HttpDelete("{courseID}")]
+    public ActionResult deleteCourse(int courseID)
+    {
+        var course = _db.Courses.FirstOrDefault(x => x.CourseId == courseID);
+
+        if (course == null)
+        {
+            return BadRequest("Course not found");
+        }
+
+        _db.Courses.Remove(course);
+        _db.SaveChanges();
+
+        return Ok("Course removed successfully");
     }
 }
