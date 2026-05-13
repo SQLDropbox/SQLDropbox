5677ab1f096ff5dfc1274d6be442e79a438c946c
Lander123Lander <lander@dirix-philips.be>
2026-05-13 13:56:23 +0200

    courseDTO

---
+++ sql-dropbox-backend/SQLDropbox/DTO/CourseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace SQLDropbox.DTO
+{
+    public class CourseDTO
+    {
+        public string? CourseNameEN { get; set; }
+        public string? CourseNameNL { get; set; }
+        public string? CourseDescriptionEN { get; set; }
+        public string? CourseDescriptionNL { get; set; }
+        public string? Lecturer { get; set; }
+        public DateTime? Deadline { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
