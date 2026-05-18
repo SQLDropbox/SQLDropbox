7f9ef2a0eb34b0071d2a8cbcab45890a97c98e50
J-VD-M <r0950937@ucll.be>
2026-05-18 21:54:52 +0200

    added password setup endpoints for lecturer and student (not complete)

--- sql-dropbox-backend/SQLDropbox/Controllers/AuthController.cs
+++ sql-dropbox-backend/SQLDropbox/Controllers/AuthController.cs
@@ -8,27 +8,42 @@ namespace SQLDropbox.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class AuthController : ControllerBase
+public class AuthController(AppDbContext db) : ControllerBase
 {
-    private readonly AppDbContext _db;
-    public AuthController(AppDbContext db)
-    {
-        _db = db;
-    }
+    private readonly AppDbContext _db = db;
 
-    [HttpGet("{guid}")]
-    public async Task<ActionResult> SetupPassword(string guidStr)
+    [HttpGet("lecturer/{guid}")]
+    public async Task<ActionResult> SetupLecturerPassword(string guidStr)
     {
         if (Guid.TryParse(guidStr, out Guid guid))
             return BadRequest("Not a valid GUID.");
 
-        Student student = await _db.Students.FindAsync(guid);
+        Lecturer? lecturer = await _db.Lecturers.FindAsync(guid);
 
-        if (student == null) return BadRequest("Student does not exist.");
+        if (lecturer == null)
+            return BadRequest("Lecturer does not exist.");
 
-        if (student.Password != null) return BadRequest("Student is already setup.");
+        if (lecturer.Password != null)
+            return BadRequest("Lecturer is already setup.");
 
-        return Ok("Ok");
-       
- ;   }
+        return Ok($"Hello there {lecturer.FirstName} please set your password");
+    }
+
+    [HttpGet("student/{guid}")]
+    public async Task<ActionResult> SetupStudentPassword(string guidStr)
+    {
+        if (Guid.TryParse(guidStr, out Guid guid))
+            return BadRequest("Not a valid GUID.");
+
+        Student? student = await _db.Students.FindAsync(guid);
+
+        if (student == null)
+            return BadRequest("Student does not exist.");
+
+        if (student.Password != null)
+            return BadRequest("Student is already setup.");
+
+        return Ok($"Hello there {student.FirstName} please set your password");
+    }
+
 }
