649fb5d3399e64fd465a17dd1607d3d3473e57b2
Lander123Lander <lander@dirix-philips.be>
2026-05-22 12:33:04 +0200

    fix?

--- sql-dropbox-frontend/utils/authUtils.ts
+++ sql-dropbox-frontend/utils/authUtils.ts
@@ -2,11 +2,21 @@ import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.share
 
 const login = (router: AppRouterInstance, token: string) => {
     document.cookie = `token=${token}; path=/; max-age=${60 * 60 * 24 * 14}; SameSite=Strict`;
+
+    setTimeout(() => {
+        router.push("/");
+    }, 50);
+
     router.push("/");
-}
+};
 
 const logout = (router: AppRouterInstance) => {
     document.cookie = "token=; path=/; max-age=0; SameSite=Strict";
+
+    setTimeout(() => {
+        router.push("/");
+    }, 50);
+    
     router.push("/login");
 };
 
