cc338b9b05e91f96c2a534e99812b0b7e2b76fbf
Lander123Lander <lander@dirix-philips.be>
2026-05-26 12:57:49 +0200

    addStudentToCourse endpoint

--- sql-dropbox-backend/SQLDropbox/Controllers/UserController.cs
+++ sql-dropbox-backend/SQLDropbox/Controllers/UserController.cs
@@ -16,17 +16,21 @@ namespace SQLDropbox.Controllers
         private readonly AppDbContext _db = db;
         private readonly PasswordService _passwordService = passwordService;        
 
-        [Authorize(Roles = "Admin")]
-        [HttpPost("create")]
-        public async Task<ActionResult> CreateStudent(StudentDTO dto)
+        [Authorize(Roles = "Admin,Lecturer")]
+        [HttpPost("studentCourse/{courseId}")]
+        public async Task<ActionResult> AddStudentToCourse(string courseId, StudentDTO dto)
         {
             try
             {
+                // TODO: update to check if the user already exists but in a different course, assign to this course, replace name and email?
                 User? user = await _db.Users.FirstOrDefaultAsync(u => u.UserCode == dto.UserCode);
-                if (user != null) throw new Exception("A user with this code already exists");
+                if (user != null) return BadRequest("A user with this code already exists"); 
 
                 if (dto.UserCode == null || dto.Email == null)
                     return BadRequest("Usercode and Email are required.");
+
+                Course? course = await _db.Courses.FirstOrDefaultAsync(x => x.CourseId == courseId);
+                if (course == null) return BadRequest("Course not found");
 
                 User newStudent = new()
                 {
@@ -34,14 +38,15 @@ namespace SQLDropbox.Controllers
                     FirstName = dto.FirstName,
                     LastName = dto.LastName,
                     Email = dto.Email,
+                    StudentCourses = [course],
                     Role = Role.Student,
-                    CreatedAt = DateTime.UtcNow
+                    CreatedAt = DateTime.UtcNow,
                 };
 
                 _db.Users.Add(newStudent);
                 await _db.SaveChangesAsync();
 
-                return Ok($"Account created for user with code {newStudent.UserCode}.");
+                return Ok();
             }
             catch (Exception ex)
             {
