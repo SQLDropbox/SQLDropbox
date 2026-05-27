d53a0bf3a195ece97632863f1d36f6f280194e5a
Lander123Lander <lander@dirix-philips.be>
2026-05-27 09:40:45 +0200

    read expiration time from JWT

--- sql-dropbox-frontend/utils/authUtils.ts
+++ sql-dropbox-frontend/utils/authUtils.ts
@@ -1,10 +1,14 @@
 import { authService } from "@/services/authService";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-const login = async (router: AppRouterInstance, emailOrCode: string, password: string) => {
+const login = async (
+    router: AppRouterInstance,
+    emailOrCode: string,
+    password: string,
+) => {
     const response = await authService.login(emailOrCode, password);
 
-    document.cookie = `token=${response.token}; path=/; max-age=${10}; SameSite=Strict`;
+    setJWTCookie(response.token);
 
     setTimeout(() => {
         router.push("/");
@@ -14,7 +18,8 @@ const login = async (router: AppRouterInstance, emailOrCode: string, password: s
 const logout = async (router: AppRouterInstance) => {
     try {
         await authService.logout();
-        document.cookie = "token=; path=/; max-age=0; SameSite=Strict";
+
+        setJWTCookie(null);
 
         setTimeout(() => {
             router.push("/login");
@@ -24,7 +29,31 @@ const logout = async (router: AppRouterInstance) => {
     }
 };
 
+export const setJWTCookie = (token: string | null = null) => {
+    try {
+        if (!token) {
+            document.cookie = "token=; path=/; max-age=0; SameSite=Strict";
+            return;
+        }
+
+        const payload = JSON.parse(atob(token.split(".")[1]));
+
+        const exp = payload.exp;
+
+        if (!exp) {
+            throw new Error("Token missing exp claim");
+        }
+
+        const maxAge = exp - Math.floor(Date.now() / 1000);
+
+        document.cookie = `token=${token}; path=/; max-age=${maxAge}; SameSite=Strict`;
+    } catch (err) {
+        console.error("Invalid token:", err);
+    }
+};
+
 export const authUtils = {
     login,
     logout,
+    setJWTCookie,
 };
