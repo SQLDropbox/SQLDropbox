4f05b9df39c99ae3d010cf615f5aabbe79498cf3
r0947044 <r0947044@ucll.be>
2026-06-01 15:56:47 +0200

    backend logic works for adding a schema

--- sql-dropbox-backend/SQLDropbox/Controllers/SchemaDbController.cs
+++ sql-dropbox-backend/SQLDropbox/Controllers/SchemaDbController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using SQLDropbox.Data;
 using SQLDropbox.Models;
+using SQLDropbox.DTO;
 
 namespace SQLDropbox.Controllers
 {
@@ -30,6 +31,47 @@ namespace SQLDropbox.Controllers
                 .ToListAsync();
 
             return Ok(schemas);
+        }
+
+        [HttpPost("/Schema")]
+        public async Task<ActionResult> CreateSchema([FromForm] SchemaDTO dto)
+        {
+            string? imageFileName = null;
+
+            if (dto.Image != null)
+            {
+                var uploadsFolder = Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "wwwroot",
+                    "schema-images");
+
+                Directory.CreateDirectory(uploadsFolder);
+
+                var extension = Path.GetExtension(dto.Image.FileName);
+                imageFileName = $"{Guid.NewGuid()}{extension}";
+
+                var filePath = Path.Combine(uploadsFolder, imageFileName);
+
+                using var stream = new FileStream(filePath, FileMode.Create);
+                await dto.Image.CopyToAsync(stream);
+            }
+
+            var schema = new Schema
+            {
+                SchemaName = dto.SchemaName,
+                SchemaImage = imageFileName,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _context.Set<Schema>().Add(schema);
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                schema.SchemaId,
+                schema.SchemaName,
+                schema.SchemaImage
+            });
         }
     }
 }
--- sql-dropbox-backend/SQLDropbox/DTO/SchemaDTO.cs
+++ sql-dropbox-backend/SQLDropbox/DTO/SchemaDTO.cs
@@ -1,6 +1,11 @@
-﻿namespace SQLDropbox.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SQLDropbox.DTO
 {
     public class SchemaDTO
     {
+        [Required]
+        public string SchemaName { get; set; } = null!;
+        public IFormFile? Image { get; set; }
     }
 }
